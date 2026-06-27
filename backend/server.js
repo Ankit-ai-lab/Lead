870b0df4e3d984761528dc3cd674e1ec736be0cc
Ankit-ai-lab <ankitguptame98@gmail.com>
2026-06-27 20:49:50 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ["https://lead-mu-three.vercel.app", "http://localhost:3000"],
+  origin: ["https://lead-flhx.vercel.app", "http://localhost:3000"],
   credentials: true
 }));
 app.use(express.json()); 
